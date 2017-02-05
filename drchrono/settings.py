4c2d51c079771f19c0dcad05ed619c356b1f6622
jgong1 <michaelgongjian@gmail.com>
2017-02-05 15:59:01 -0500

    setting change for authentication

--- drchrono/settings.py
+++ drchrono/settings.py
@@ -13,6 +13,9 @@ https://docs.djangoproject.com/en/1.8/ref/settings/
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
 
+# Authentication Settings - Keep Private for SECURITY
+from social_auth_drchrono.auth_config import *
+
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
@@ -109,3 +112,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+
