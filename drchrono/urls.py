251e6e21ce62693d28757391c5c460d78907b57b
jgong1 <michaelgongjian@gmail.com>
2017-02-05 15:59:48 -0500

    modified url for authentication

--- drchrono/urls.py
+++ drchrono/urls.py
@@ -7,5 +7,8 @@ import views
 urlpatterns = [
     url(r'^$', TemplateView.as_view(template_name='index.html'), name='home'),
 
+    # url(r'^$', 'drchrono.views.login', name='home'),
     url(r'', include('social.apps.django_app.urls', namespace='social')),
+    url(r'^main/$', 'drchrono.views.main', name='main')
+
 ]
