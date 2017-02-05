39c9c5eb29153b936810e3bcc6d3c6275eeb828c
jgong1 <michaelgongjian@gmail.com>
2017-02-05 16:00:08 -0500

    modified views for authentication

--- drchrono/views.py
+++ drchrono/views.py
@@ -1,6 +1,8 @@
 
-
+from django.shortcuts import render_to_response, redirect, render
 from django.http import HttpResponse
 from models import *
 
-
+def main(request):
+    print 'Redirect to main page with request: ', request
+    return render(request, 'main.html')
