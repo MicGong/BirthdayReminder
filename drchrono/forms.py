fd2e8639b6deceb7cc38e6911a59f1aa5d9671cf
jgong1 <michaelgongjian@gmail.com>
2017-02-12 21:29:34 -0500

    EmailForm for email validation

--- drchrono/forms.py
+++ drchrono/forms.py
@@ -1,3 +1,21 @@
 from django import forms
 
-# forms go here
+# class SearchForm(forms.Form):
+#     """Django form of search"""
+#     first_name = forms.CharField(max_length=255, required=False)
+#     last_name = forms.CharField(max_length=255, required=False)
+#     gender = forms.ChoiceField(choices=(('M','Male'),('F','Female')), required=False)
+#     date_of_birth = forms.DateField(input_formats='%m/%d/%Y', required=False)
+
+#     def clean(self):
+#         cleaned_data = super(SearchForm, self).clean()
+#         return cleaned_data
+
+class EmailForm(forms.Form):
+    """Email Django form"""
+    title = forms.CharField(max_length=300, required=False)
+    body = forms.CharField(required=False)
+
+    def clean(self):
+        cleaned_data = super(EmailForm, self).clean()
+        return cleaned_data
