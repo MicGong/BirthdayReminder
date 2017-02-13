14780a64d0dd713bb3bfef3cf8ce3cad0d999697
jgong1 <michaelgongjian@gmail.com>
2017-02-12 21:31:51 -0500

    add email related urls

--- drchrono/urls.py
+++ drchrono/urls.py
@@ -6,9 +6,16 @@ import views
 
 urlpatterns = [
     url(r'^$', TemplateView.as_view(template_name='index.html'), name='home'),
-
-    # url(r'^$', 'drchrono.views.login', name='home'),
     url(r'', include('social.apps.django_app.urls', namespace='social')),
-    url(r'^main/$', 'drchrono.views.main', name='main')
+    
+    url(r'^main/$', 'drchrono.views.main', name='main'),
 
+    # url(r'^search/$', 'drchrono.views.search', name='search'),
+
+    # url(r'', 'drchrono.views.login_failed', name='loginfailed')
+
+    url(r'^main/edit-email/$', 'drchrono.views.edit_email', name='editemail'),
+    url(r'^main/send-email-default/$', 'drchrono.views.send_email_default', name='sendemaildefault'),
+    url(r'main/send-email-custom/$', 'drchrono.views.send_email_custom', name='sendemailcustom'),
+    url(r'^logout/$', 'drchrono.views.logout', name='logout'),
 ]
